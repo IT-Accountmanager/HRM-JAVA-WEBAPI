46df108bdc896d3b5ece62509477969f403b5b42
Pritipragyan.b <Pritipragyan.b@EISBL038.EG.AC>
2023-09-06 14:30:54 +0530

    Merge branch 'kartik' of https://github.com/IT-Accountmanager/HRM-JAVA-WEBAPI.git into Priti_Pragyan

--- src/main/java/com/hrm/main/models/OnboardingEmployeeSummary.java
+++ src/main/java/com/hrm/main/models/OnboardingEmployeeSummary.java
@@ -15,28 +15,33 @@ public class OnboardingEmployeeSummary {
 	@SequenceGenerator(name="Summary_Id_Sequence", initialValue = 1, allocationSize = 1, sequenceName = "Summary_Id_Sequence")
 	
 	private int id;
-	private String jobTitle;
-	private long candidateId;
-	public long getCandidateId() {
-		return candidateId;
-	}
-	public void setCandidateId(long candidateId) {
-		this.candidateId = candidateId;
-	}
+	private long employeeid;
+	private String employeeName;
+	private long mobileNumber;
+	private String emailId;
+	private String dateOfJoining;
+	private String designation;
+	private String department;
+	private String status;
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	public String getJobTitle() {
-		return jobTitle;
+	public long getEmployeeid() {
+		return employeeid;
 	}
-	public void setJobTitle(String jobTitle) {
-		this.jobTitle = jobTitle;
+	public void setEmployeeid(long employeeid) {
+		this.employeeid = employeeid;
 	}
-	
-		public long getMobileNumber() {
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public long getMobileNumber() {
 		return mobileNumber;
 	}
 	public void setMobileNumber(long mobileNumber) {
@@ -48,11 +53,23 @@ public class OnboardingEmployeeSummary {
 	public void setEmailId(String emailId) {
 		this.emailId = emailId;
 	}
-	public String getLocation() {
-		return location;
+	public String getDateOfJoining() {
+		return dateOfJoining;
 	}
-	public void setLocation(String location) {
-		this.location = location;
+	public void setDateOfJoining(String dateOfJoining) {
+		this.dateOfJoining = dateOfJoining;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
 	}
 	public String getStatus() {
 		return status;
@@ -60,17 +77,6 @@ public class OnboardingEmployeeSummary {
 	public void setStatus(String status) {
 		this.status = status;
 	}
-	private String candidateName;
-	public String getCandidateName() {
-		return candidateName;
-	}
-	public void setCandidateName(String candidateName) {
-		this.candidateName = candidateName;
-	}
-	private long mobileNumber;
-	private String emailId;
-	private String location;
-	private String status;
-
+	
 }
 
