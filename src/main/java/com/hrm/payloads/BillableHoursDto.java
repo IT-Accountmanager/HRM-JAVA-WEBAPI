549fc90507452021e46a728e7adecf931eb984e5
Lenovo <Lenovo@RC>
2024-03-18 13:09:45 +0530

    fff

--- src/main/java/com/hrm/payloads/BillableHoursDto.java
+++ src/main/java/com/hrm/payloads/BillableHoursDto.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class BillableHoursDto {
-//	private List<LocalDate> listOfDate;
 	private LocalDate date;
 	private float productionHours;
 	private float otherHours;
